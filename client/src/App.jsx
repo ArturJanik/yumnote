4b6ace801d36fecf7cf6f3c58149d9b14bbb4af0
Artur Janik <sidsky@o2.pl>
2018-12-23 00:11:19 +0100

    Added icon lib config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,18 @@ import {
   withRouter
 } from 'react-router-dom';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+
 import Layout from './hoc/Layout/Layout';
 import NoMatch from './components/Error/NoMatch';
 import ArticleList from './containers/ArticleList/ArticleList';
 import Article from './containers/Article/Article';
+import AddArticle from './containers/AddArticle/AddArticle';
+
+library.add( 
+  faCalendarAlt,
+);
 
 class App extends Component {
   render() {
@@ -16,6 +24,7 @@ class App extends Component {
       <Switch>
         <Route path="/articles" exact component={ArticleList} />
         <Route path="/articles/:articleId(\d+)" component={Article} />
+        <Route path="/articles/new" exact component={AddArticle} />
         <Route path="/" exact component={ArticleList} />
         <Route component={NoMatch} />
       </Switch>
