6f5894020fff5691eabebc555aa4db68ff3295b8
Artur Janik <sidsky@o2.pl>
2018-12-30 18:55:32 +0100

    Added menu show logic

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -5,12 +5,25 @@ import Logo from './Logo/Logo';
 import Menu from './Menu/Menu';
 
 class Header extends Component {
+  state = {
+    expandMainMenu: false,
+  }
+
+  mainMenuToggleHandler = () => {
+    this.setState((prevState) => {
+      return { expandMainMenu: !prevState.expandMainMenu }
+    })
+  }
+
   render() {
     return (
       <header>
         <div className={styles.wrapper}>
           <Logo />
-          <Menu />
+          <Menu
+            expand={this.state.expandMainMenu}
+            clicked={this.mainMenuToggleHandler}
+          />
         </div>
       </header>
     );
--- client/src/components/Header/Menu/Menu.jsx
+++ client/src/components/Header/Menu/Menu.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './Menu.css';
 
 class Menu extends Component {
-  render() {
+  state = {
+    expand: false
+  }
+
+  menuToggleHandler = () => {
+    this.setState((prevState) => {
+      return { expand: !prevState.expand }
+    })
+  }
+
+  menuHideHandler = () => {
+    this.setState({ expand: false })
+  }
+
+  render(){
     return (
-      <div className={styles['menu-main']}>Menu</div>
-    );
+      <div className={styles['menu--main']}>
+        <p onClick={this.menuToggleHandler}>MainMenu</p>
+        { this.state.expand ?
+        (<ul className={styles['menu-items']} onClick={this.menuHideHandler}>
+          <li className={styles['menu-item']}><NavLink exact to="/">Menu Item #1</NavLink></li>
+          <li className={styles['menu-item']}><NavLink exact to="/">Menu Item #2</NavLink></li>
+          <li className={styles['menu-item']}><NavLink exact to="/">Menu Item #3</NavLink></li>
+          <li className={styles['menu-item']}><NavLink exact to="/">Menu Item #4</NavLink></li>
+        </ul>) : null
+        }
+      </div>
+    )
   }
 }
 
