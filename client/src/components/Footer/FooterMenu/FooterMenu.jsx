2104ca2bf6d13ddd527ef2bdc0f107ccc6dc51a9
Artur Janik <sidsky@o2.pl>
2018-12-31 17:05:40 +0100

    Added footer menu component

--- client/src/components/Footer/FooterMenu/FooterMenu.jsx
+++ client/src/components/Footer/FooterMenu/FooterMenu.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './FooterMenu.css';
 
 class FooterMenu extends Component {
   render() {
     return (
-      <div className={styles['menu-footer']}>FooterMenu</div>
+      <div className={styles['menu--footer']}>
+        <ul className={styles['menu-items']} onClick={this.menuHideHandler}>
+          <li className={styles['menu-title']}>Menu</li>
+          <li className={styles['menu-item']}><NavLink exact to="/">Menu Item #1</NavLink></li>
+          <li className={styles['menu-item']}><NavLink exact to="/">Menu Item #2</NavLink></li>
+          <li className={styles['menu-item']}><NavLink exact to="/">Menu Item #3</NavLink></li>
+          <li className={styles['menu-item']}><NavLink exact to="/">Menu Item #4</NavLink></li>
+        </ul>
+      </div>
     );
   }
 }
