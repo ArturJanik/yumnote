722c27cf8d852a49dd3311e7ef6c479f0d3f964e
Artur Janik <sidsky@o2.pl>
2019-01-04 16:58:35 +0100

    Added logo placeholder

--- client/src/components/Header/Logo/Logo.jsx
+++ client/src/components/Header/Logo/Logo.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import styles from './Logo.css';
+import logo from './logo.png';
 
 class Logo extends Component {
   render() {
     return (
-      <div className={styles.logo}>Logo</div>
+      <div className={styles.logo}><img src={logo} alt="Cebu.love - Kocham promocje!" /></div>
     );
   }
 }
