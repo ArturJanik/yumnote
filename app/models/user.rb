ba371988fbd1b2ef736fb008d547e5c15bc1ce1a
Artur Janik <sidsky@o2.pl>
2019-01-07 19:03:29 +0100

    Add User model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ApplicationRecord
+  validates_uniqueness_of :username, :email
+
+  validates :username, length: { minimum: 4 }
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create }
+
+  has_secure_password validations: false
+  validates :password, length: { minimum: 6 }, allow_nil: true
+  validates_presence_of :password, :email, :username, on: :create
+  has_secure_token :auth_token
+  
+  has_many :articles
+
+  def invalidate_token
+    self.update_columns(auth_token: nil)
+  end
+
+  def self.validate_login(email, password)
+    user = find_by(email: email)
+    if user && user.authenticate(password)
+      user
+    end
+  end
 end
