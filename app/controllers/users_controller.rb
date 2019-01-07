311e9429d173c995ad65dd5ccded56f909d8b3d9
Artur Janik <sidsky@o2.pl>
2019-01-07 20:25:38 +0100

    Added users controller for auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApiController
+  before_action :require_login, except: [:create]
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: { token: user.auth_token, username: user.username }
+    else
+      render json: { errors: { details: user.errors } }, status: 422
+    end
+  end
+
+  def profile
+    user = User.find_by_username(params[:username])
+    if user
+      user_promos = user.promos
+      render json: {
+        user: {
+          username: user.username
+        },
+        promos: user_promos
+      }
+    else
+      render json: { errors: { details: user.errors } }, status: 404
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
