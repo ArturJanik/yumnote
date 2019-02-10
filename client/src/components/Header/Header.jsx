24b3fa79e5e526eb2236218293509f95fd76bf2e
Artur Janik <sidsky@o2.pl>
2019-02-10 22:10:01 +0100

    Fixed header menu auth check + deleted old Auth comp

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import styles from './Header.css';
 
 import Logo from './Logo/Logo';
 import Menu from './Menu/Menu';
+import MobileMenu from './MobileMenu/MobileMenu';
 
 class Header extends Component {
   state = {
@@ -14,20 +16,31 @@ class Header extends Component {
       return { expandMainMenu: !prevState.expandMainMenu }
     })
   }
-
+  
   render() {
     return (
       <header>
         <div className={styles.wrapper}>
           <Logo />
           <Menu
+            isAuth={this.props.isAuthenticated}
+            currentUser={this.props.currentUserName}
+          />
+          {/* <MobileMenu 
             expand={this.state.expandMainMenu}
             clicked={this.mainMenuToggleHandler}
-          />
+          /> */}
         </div>
       </header>
     );
   }
 }
 
-export default Header;
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+    currentUserName: state.auth.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/hoc/Layout/Layout.jsx
+++ client/src/hoc/Layout/Layout.jsx
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-import { connect } from 'react-redux';
 
 class Layout extends Component {
   render() {
     return (
       <React.Fragment>
-        <Header
-          isAuth={this.props.isAuthenticated} 
-          currentUser={this.props.currentUserName} 
-        />
+        <Header />
         {this.props.children}
         <Footer />
       </React.Fragment>
     );
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    isAuthenticated: state.auth.token !== null,
-    currentUserName: state.auth.currentUser
-  }
-}
-
-export default connect(mapStateToProps)(Layout);
+export default Layout;
