e3672da235a765522304582ea8f6b15233cadf75
Artur Janik <sidsky@o2.pl>
2019-02-12 14:08:08 +0100

    404 page styles

--- client/src/components/Error/NoMatch.jsx
+++ client/src/components/Error/NoMatch.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import styles from './NoMatch.css';
+import nomatch from './../../assets/images/error_404.jpg';
 
-const NoMatch = (props) => <div>Error 404</div>;
+const NoMatch = (props) => <div className={styles.error404}><img src={nomatch} alt="Error 404 - page not found" /></div>;
 
 export default NoMatch;
