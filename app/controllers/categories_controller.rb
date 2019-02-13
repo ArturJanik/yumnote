e435d62c869d7bcb136a94a07bac44c4ccc7683b
Artur Janik <sidsky@o2.pl>
2019-02-13 12:50:13 +0100

    Removed redundant auth from category ctrl

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApiController
-  before_action :require_login
-  
+
   def index
     categories = Category.all_with_basic_data
     render json: { categories: categories }
