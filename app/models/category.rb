a715190adda309e12b58282d1242bb87eb8c65d0
Artur Janik <sidsky@o2.pl>
2019-02-13 12:08:52 +0100

    Added basic scope to reduce returned category data

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,6 @@ class Category < ApplicationRecord
   has_many :products
   
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+
+  scope :all_with_basic_data, -> { where(active: true).select("categories.id, categories.name").order(name: :desc) }
 end
