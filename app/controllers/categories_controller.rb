fa4b06df1f04b85dbadf2a843b1f159c5b499f3b
Artur Janik <sidsky@o2.pl>
2019-02-13 12:09:14 +0100

    Added categories controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,8 @@
+class CategoriesController < ApiController
+  before_action :require_login
+  
+  def index
+    categories = Category.all_with_basic_data
+    render json: { categories: categories }
+  end
+end
