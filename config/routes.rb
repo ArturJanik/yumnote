f1c045de0bf79ea3f4c2be36f938c098a23a38a1
Artur Janik <sidsky@o2.pl>
2019-02-21 12:14:27 +0100

    Restructured route and removed redundancies

--- config/routes.rb
+++ config/routes.rb
@@ -5,21 +5,22 @@ Rails.application.routes.draw do
   scope '/api' do
     post '/login' => "sessions#create"
     delete '/logout' => "sessions#destroy"
+    
+    resources :users, only: [:create]
+    # resources :users, except: [:index,:destroy] do
+      # get 'statistics' => 'users#statistics'
+    # end
+    # get '/profile' => "users#profile"
 
     get '/foodnotes/today' => 'foodnotes#index'
     get '/foodnotes/yesterday' => 'foodnotes#yesterday'
     get '/foodnotes/:day' => 'foodnotes#show'
     resources :foodnotes, only: [:create,:update,:destroy]
-    
-    resources :users, except: [:index,:destroy] do
-      # get 'statistics' => 'users#statistics'
-    end
-    # get '/profile' => "users#profile"
 
-    # resources :categories, only: [:index] do
-    #   resources :products
-    # end
-    resources :categories, only: [:index]
-    resources :products, only: [:index,:show,:create,:update,:destroy]
+    resources :categories, only: [:index] do
+      resources :products, only: [:index]
+    end
+    get '/products/currentuser' => 'products#userproducts'
+    get '/products/latest' => 'products#latest'
   end
 end
