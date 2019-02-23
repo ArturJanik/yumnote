479f0c970acadcdeaaeee4b3c3302e91c0f3c606
Artur Janik <sidsky@o2.pl>
2019-02-23 14:04:18 +0100

    Created form for Category in admin

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -1,15 +1,29 @@
 ActiveAdmin.register Category do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  permit_params :name, :active, :order, :color, :slug, :parent_id
+
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+      f.input :name
+      f.input :active, as: :boolean
+      f.input :parent
+    end
+    f.actions
+  end
+
+  before_create do |category|
+    slug = category.name.parameterize
+
+    i = 1
+    loop do
+      if Category.where(slug: slug).exists?
+        slug = category.title.parameterize + "-#{i}"
+        i+=1
+      else
+        category.slug = slug
+        break
+      end
+    end
+  end
 
 end
