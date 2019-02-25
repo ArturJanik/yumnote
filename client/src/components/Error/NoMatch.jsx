38f7b2b62f8f5db5730e42abb329f27b71a47fe1
Artur Janik <sidsky@o2.pl>
2019-02-25 14:21:08 +0100

    Updated React to 16.8.3

--- client/src/components/Error/NoMatch.jsx
+++ client/src/components/Error/NoMatch.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './NoMatch.css';
 import nomatch from './../../assets/images/error_404.jpg';
 
-const NoMatch = (props) => <div className={styles.error404}><img src={nomatch} alt="Error 404 - page not found" /></div>;
+const NoMatch = (props) => {
+  useEffect(() => {
+    document.title = '404 - page not found - calories.today';
+  })
+  return(
+    <div className={styles.error404}><img src={nomatch} alt="Error 404 - page not found" /></div>
+  )
+};
 
 export default NoMatch;
